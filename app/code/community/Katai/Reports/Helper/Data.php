336b4118f43eae04ab04d093e8ff1c039afa8cd6
Mitch Vanderlinden <m.vanderlinden@geschenkidee.de>
2015-09-15 16:06:18 +0200

    added helper

---
+++ app/code/community/Katai/Reports/Helper/Data.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * File: Data.php
+ *
+ * User: Mitch Vanderlinden
+ * email: magento@mitchvdl.be
+ * Date: 15.09.15 16:05
+ * Package: Katai_Reports
+ */
+
+class Katai_Reports_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
