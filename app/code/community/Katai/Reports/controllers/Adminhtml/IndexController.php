af101b9e600b4fcb2276ffefd4d47df494871ade
Mitch Vanderlinden <m.vanderlinden@geschenkidee.de>
2015-09-15 16:58:48 +0200

    added admin menu entry to reports

---
+++ app/code/community/Katai/Reports/controllers/Adminhtml/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * File: IndexController.php
+ *
+ * User: Mitch Vanderlinden
+ * email: magento@mitchvdl.be
+ * Date: 15.09.15 16:52
+ * Package: Katai_Reports
+ */
+
+class Katai_Reports_Adminhtml_IndexController extends Mage_Adminhtml_Controller_Action
+{
+    /**
+     * Check ACL permissins
+     *
+     * @return bool
+     */
+    protected function _isAllowed()
+    {
+        return Mage::getSingleton('admin/session')->isAllowed('katai/reports');
+    }
+}
